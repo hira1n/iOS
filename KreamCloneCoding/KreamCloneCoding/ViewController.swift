d838c9ef82746f2b9281f7a679a4af321e56077e
won <andrew112517@naver.com>
2024-09-29 14:34:31 +0900

    [Feat] Kream클론코딩 프로젝트 세팅

---
+++ KreamCloneCoding/KreamCloneCoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KreamCloneCoding
+//
+//  Created by 김승원 on 9/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ KreamCloneCoding/KreamCloneCodingTests/KreamCloneCodingTests.swift
@@ -0,0 +1,17 @@
+//
+//  KreamCloneCodingTests.swift
+//  KreamCloneCodingTests
+//
+//  Created by 김승원 on 9/29/24.
+//
+
+import Testing
+@testable import KreamCloneCoding
+
+struct KreamCloneCodingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
